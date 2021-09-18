c71dcf1b9ae9dd62e080e4c4c6d65b2368f9ea87
mirzaAsca <asca_mirza7@hotmail.com>
2021-09-18 10:18:46 +0200

    1st push

---
+++ ex09/src/index.js
@@ -0,0 +1,11 @@
+function main() {
+    // Only change code below this line
+    var lastName = "Doe";
+    var firstLetterOfLastName = " ";
+    firstLetterofLastName = lastName[0];
+    // Only change code above this line
+    return firstLetterofLastName;
+}
+
+console.log(main());
+module.exports = main;
